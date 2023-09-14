7007e1aff84baf0beb6668178a7d3dcd08ecf8f4
Elias <eeschmoelz@gmail.com>
2023-09-14 23:07:49 +0100

    fix: motion types

--- packages/ventana-react/src/motion.ts
+++ packages/ventana-react/src/motion.ts
@@ -1,4 +1,4 @@
-export type SpringConfig = {
+export type MotionValues = {
   initial: number;
   current: number;
   dest: number;
@@ -7,34 +7,46 @@ export type SpringConfig = {
   b: number;
 };
 
-export type ElementSpringConfig = {
-  x: SpringConfig;
-  y: SpringConfig;
-  w: SpringConfig;
-  h: SpringConfig;
-  opacity: SpringConfig;
-  scaleX: SpringConfig;
-  scaleY: SpringConfig;
+export type ElementMotionConfig = {
+  x: MotionValues;
+  y: MotionValues;
+  w: MotionValues;
+  h: MotionValues;
+  opacity: MotionValues;
+  scaleX: MotionValues;
+  scaleY: MotionValues;
+  // top: MotionValues;
+  // left: MotionValues;
 };
 
-export type Cache = Map<Element, ElementSpringConfig>;
+export type MotionCustomValues = {
+  stiffness?: number;
+  dampness?: number;
+};
+
+export type ElementMotionProp = Partial<Record<keyof ElementMotionConfig, MotionCustomValues>>;
+
+export type Cache = Map<Element, ElementMotionConfig>;
 
 // 60fps = 16.666ms per frame
 export const MOTION_STEP = 17;
 
+const DEFAULT_STIFFNESS = 150;
+const DEFAULT_DAMPNESS = 15;
+
 // k = stiffness, b = damping. Higher k = more springy, higher b = more dampened
-export const spring = (pos: number, v = 0, k = 150, b = 15) => {
+export const spring = (pos: number, motion?: MotionCustomValues) => {
   return {
     initial: pos,
     current: pos,
     dest: pos,
-    v,
-    k,
-    b,
-  } satisfies SpringConfig;
+    v: 0,
+    k: motion?.stiffness ?? DEFAULT_STIFFNESS,
+    b: motion?.dampness ?? DEFAULT_DAMPNESS,
+  } satisfies MotionValues;
 };
 
-export const springStep = (config: SpringConfig) => {
+export const springStep = (config: MotionValues) => {
   // https://blog.maximeheckel.com/posts/the-physics-behind-spring-animations/
   // this seems inspired by https://github.com/chenglou/react-motion/blob/9e3ce95bacaa9a1b259f969870a21c727232cc68/src/stepper.js
   const t = MOTION_STEP / 1000; // convert to seconds for the physics equation
@@ -46,12 +58,14 @@ export const springStep = (config: SpringConfig) => {
   const newV = v + a * t;
   const newPos = current + newV * t;
 
+  //console.table({ current, dest, v, k, b, Fspring, Fdamper, a, newV, newPos });
+
   config.current = newPos;
   config.v = newV;
 };
 
 // end spring
-export const springGoToEnd = (config: SpringConfig) => {
+export const springGoToEnd = (config: MotionValues) => {
   config.current = config.dest;
   config.v = 0;
 };
